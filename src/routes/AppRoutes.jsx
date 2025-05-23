4c915a95470ecbe691c62612839b0a75423ed7c3
Mansvi <itsmansvi@gmail.com>
2025-05-23 23:14:40 +0530

    updated routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -77,7 +77,6 @@ const AppRoutes = () => {
                     </ProtectedRoute>
                   }
                 />
-                <Route path="*" element={<NotFound />} />
               </Routes>
               <Footer />
             </>
